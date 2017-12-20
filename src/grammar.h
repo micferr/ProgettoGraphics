92e1d4ddc2bb4239b2de47dbe7353b7bb9ac137b
Michele Ferraro <micfer96@gmail.com>
2017-12-20 22:27:53 +0100

    Refactoring di grammar

--- src/grammar.h
+++ src/grammar.h
@@ -3,11 +3,13 @@
 
 #include <vector>
 #include <map>
+#include <list>
 
 namespace rekt {
 
 using std::vector;
 using std::map;
+using std::list;
 
 template<typename T>
 class grammar {
@@ -25,7 +27,7 @@ public:
 	}
 
     /**
-     * TODO: docs
+     * Adds multiple production rules
      */
     void add_rules(const T& L, const vector<vector<T>>& R) {
 		for (auto& r : R) {
@@ -36,28 +38,26 @@ public:
     /**
      * Returns a random word from the grammar by using
      * leftmost derivation.
-	 * 
-	 * TODO: vector -> list
      */
     vector<T> produce() {
-		vector<T> word;
+		list<T> word;
 		word.push_back(S);
 
-		for (auto i = 0u; i < word.size();) {
-			auto& c = word[i]; // current symbol
+		for (auto it = word.begin(); it != word.end();) {
+			auto& c = *it; // current symbol
 			auto prod = prods.find(c);
 			if (prod != prods.end()) { // c is a variable
 				auto& prod_rules = (*prod).second;
 				auto& substitute = prod_rules[rand() % prod_rules.size()];
-				word.erase(word.begin() + i);
-				word.insert(word.begin() + i, substitute.begin(), substitute.end());
+				it = word.erase(it);
+				it = word.insert(it, substitute.begin(), substitute.end());
 			}
 			else { // c is a term
-				i++;
+				it++;
 			}
 		}
 
-		return word;
+		return vector<T>(word.begin(), word.end());
 	}
 };
 
