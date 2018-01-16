c91de7d80cfa2108b526f993dd8a1302f7c1b822
Michele Ferraro <micfer96@gmail.com>
2018-01-16 15:47:56 +0100

    Riordinate classi

--- src/grammar.h
+++ src/grammar.h
@@ -14,68 +14,6 @@ namespace rekt {
 	using std::map;
 	using std::queue;
 
-	template<typename T>
-	class grammar {
-		T S;
-		map<T, vector<vector<T>>> prods;
-
-	public:
-		grammar(const T& S) : S(S) {}
-
-		/**
-		 * Adds a new production rule
-		 */
-		void add_rule(const T& L, const vector<T>& R) {
-			prods[L].push_back(R);
-		}
-
-		/**
-		 * Adds multiple production rules
-		 */
-		void add_rules(const T& L, const vector<vector<T>>& R) {
-			for (auto& r : R) {
-				add_rule(L, r);
-			}
-		}
-
-		bool is_terminal(const T& value) {
-			return prods.count(value) == 0;
-		}
-
-		bool is_variable(const T& value) {
-			return !is_terminal(value);
-		}
-
-		/**
-		 * Returns a random word from the grammar by using
-		 * leftmost derivation.
-		 * The word symbols are the leaves of the returned tree, in the
-		 * order determined by a DFS, minus variables V involved in a
-		 * V -> <empty> production.
-		 *
-		 * TODO: multithreaded expansion
-		 */
-		node<T>* produce() {
-			node<T>* root = new node<T>(S);
-			queue<node<T>*> nodes;
-			nodes.push(root);
-			while (!nodes.empty()) {
-				node<T>* n = nodes.front();
-				nodes.pop();
-
-				if (is_variable(n->value)) {
-					auto& prod = prods.find(n->value);
-					auto& prod_rules = (*prod).second;
-					auto& substitution = prod_rules[rand() % prod_rules.size()];
-					for (auto& child_value : substitution) {
-						n->add_child(child_value);
-						nodes.push(n->children.back());
-					}
-				}
-			}
-			return root;
-		}
-	};
 
 	class attributes {
 		map<string, int> attribs_i;
@@ -161,6 +99,69 @@ namespace rekt {
 
 		string get_attribs(const string& name, const string& default_value) {
 			return is_attribs(name) ? attribs_s[name] : default_value;
+		}
+	};
+
+	template<typename T>
+	class grammar {
+		T S;
+		map<T, vector<vector<T>>> prods;
+
+	public:
+		grammar(const T& S) : S(S) {}
+
+		/**
+		 * Adds a new production rule
+		 */
+		void add_rule(const T& L, const vector<T>& R) {
+			prods[L].push_back(R);
+		}
+
+		/**
+		 * Adds multiple production rules
+		 */
+		void add_rules(const T& L, const vector<vector<T>>& R) {
+			for (auto& r : R) {
+				add_rule(L, r);
+			}
+		}
+
+		bool is_terminal(const T& value) {
+			return prods.count(value) == 0;
+		}
+
+		bool is_variable(const T& value) {
+			return !is_terminal(value);
+		}
+
+		/**
+		 * Returns a random word from the grammar by using
+		 * leftmost derivation.
+		 * The word symbols are the leaves of the returned tree, in the
+		 * order determined by a DFS, minus variables V involved in a
+		 * V -> <empty> production.
+		 *
+		 * TODO: multithreaded expansion
+		 */
+		node<T>* produce() {
+			node<T>* root = new node<T>(S);
+			queue<node<T>*> nodes;
+			nodes.push(root);
+			while (!nodes.empty()) {
+				node<T>* n = nodes.front();
+				nodes.pop();
+
+				if (is_variable(n->value)) {
+					auto& prod = prods.find(n->value);
+					auto& prod_rules = (*prod).second;
+					auto& substitution = prod_rules[rand() % prod_rules.size()];
+					for (auto& child_value : substitution) {
+						n->add_child(child_value);
+						nodes.push(n->children.back());
+					}
+				}
+			}
+			return root;
 		}
 	};
 }
