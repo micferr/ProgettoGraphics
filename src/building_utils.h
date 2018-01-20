4eda8e62d4281c6c9f8fec54b8969e5dcc33fa75
Michele Ferraro <micfer96@gmail.com>
2018-01-20 18:27:52 +0100

    Aggiunto tetto semplice crossgabled

--- src/building_utils.h
+++ src/building_utils.h
@@ -20,12 +20,40 @@
 namespace rekt {
 
 	ygl::shape* make_roof_crossgabled_simple(
-		const std::vector<ygl::vec3f>& floor_main_points,
+		const std::vector<ygl::vec2f>& floor_main_points,
 		float floor_width,
 		float center_height,
 		float base_height = 0.f
 	) {
-		throw std::runtime_error("not implemented");
+		auto _floor_main_points = to_3d(floor_main_points);
+		ygl::shape* shp = new ygl::shape();
+		shp->pos = rekt::make_wide_line_border(floor_main_points, floor_width);
+		auto mid_point = (shp->pos[0] + shp->pos.back()) / 2.f;
+		mid_point.y += center_height;
+		shp->pos.push_back(mid_point);
+		// i goes along the right side of the widenened segmented line, j is the opposite point
+		for (int i = 0, j = shp->pos.size() - 2; i < j-2; i+=1, j-=1) {
+			auto mid_point_next = (shp->pos[i+1] + shp->pos[j-1]) / 2.f;
+			mid_point_next.y += center_height;
+			shp->pos.push_back(mid_point_next);
+			shp->quads.push_back({ j,j - 1,i + 1,i });
+			shp->quads.push_back({ j - 1,j,int(shp->pos.size() - 2),int(shp->pos.size() - 1) });
+			shp->quads.push_back({ i,i + 1,int(shp->pos.size() - 1),int(shp->pos.size() - 2) });
+			
+			mid_point = mid_point_next;
+		}
+		int floor_points = 2 * floor_main_points.size();
+		shp->triangles.push_back({ 
+			0,floor_points,floor_points-1
+		});
+		shp->triangles.push_back({ 
+			floor_points/2, int(shp->pos.size()-1), floor_points/2-1
+		});
+		if (base_height != 0.f) { // Check purely for performance
+			displace(shp->pos, { 0,base_height,0 });
+		}
+		shp->norm = ygl::compute_normals({}, shp->triangles, shp->quads, shp->pos);
+		return shp; 
 	}
 
 }
